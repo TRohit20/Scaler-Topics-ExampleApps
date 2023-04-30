47e5d1e882b94d2ffb58bbf95dcc90e3f1829a85
Rohit <108233235+TRohit20@users.noreply.github.com>
2023-04-30 13:13:50 +0530

    Cards Example App

---
+++ example_app_cards_widget/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Card Example',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Card Example'),
+        ),
+        body: Center(
+          child: Card(
+            elevation: 10, // adds a shadow effect
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0), // adds rounded corners
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const Text(
+                    'Welcome to Flutter Cards!',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                      height: 10), // adds spacing between the text and image
+                  Image.asset(
+                    'assets/logo.png', // adds an image
+                    height: 150,
+                    width: 150,
+                  ),
+                  const SizedBox(
+                      height: 10), // adds spacing between the image and button
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Click Me!'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
