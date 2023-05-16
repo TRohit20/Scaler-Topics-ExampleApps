9d08e8f782879b0771d0e3d7d95fca775015cbd6
Rohit <108233235+TRohit20@users.noreply.github.com>
2023-05-16 15:34:05 +0530

    Flutter-Themes-ExampleApp

---
+++ example_app_themes/lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeData? _currentTheme;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentTheme = ThemeData.light();
+  }
+
+  void _changeTheme(ThemeData theme) {
+    setState(() {
+      _currentTheme = theme;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: _currentTheme,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Theme Demo'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Change the Theme:',
+                style: TextStyle(fontSize: 20),
+              ),
+              const SizedBox(height: 20),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    child: const Text('Light Mode'),
+                    onPressed: () {
+                      _changeTheme(ThemeData.light());
+                    },
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    child: const Text('Dark Mode'),
+                    onPressed: () {
+                      _changeTheme(ThemeData.dark());
+                    },
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    child: const Text('Customized Theme'),
+                    onPressed: () {
+                      _changeTheme(ThemeData(primarySwatch: Colors.red
+                          // Customize other properties as needed
+                          ));
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
